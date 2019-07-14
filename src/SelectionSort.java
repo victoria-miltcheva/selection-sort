d824934376b8bd368afc1be43e132dbb21c9161c
Victoria Miltcheva <victoria-m@users.noreply.github.com>
2019-07-14 17:51:45 -0400

    Add JavaDoc comments to SelectionSort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,6 +1,16 @@
 
 public class SelectionSort {
 
+  /**
+   * 
+   * Takes in an unsorted array and a flag which indicates ascending or descending
+   * order. Next, depending on the sort order the appropriate method is called in
+   * order to sort the array using the selection sort algorithm.
+   * 
+   * @param list      Unsorted array of integers
+   * @param ascending Flag indicating if the order is ascending or descending
+   * @return
+   */
   protected static int[] sort(int[] list, boolean ascending) {
     if (list.length == 0) {
       return new int[0];
@@ -11,6 +21,16 @@ public class SelectionSort {
     return ascending ? sortAscending(list) : sortDescending(list);
   }
 
+  /**
+   * 
+   * Performs the selection sort algorithm in order to sort the given array in
+   * ascending order. First, find the index of the smallest item in the array,
+   * then swap that item with the item at the current array index. Repeat until
+   * the last index of the array is reached.
+   * 
+   * @param list Unsorted array of integers
+   * @return
+   */
   static private int[] sortAscending(int[] list) {
     int smallestIndex;
     int temp;
@@ -26,6 +46,16 @@ public class SelectionSort {
     return list;
   }
 
+  /**
+   * 
+   * Performs the selection sort algorithm in order to sort the given array in
+   * descending order. First, find the index of the largest item in the array,
+   * then swap that item with the item at the current array index. Repeat until
+   * the last index of the array is reached.
+   * 
+   * @param list Unsorted array of integers
+   * @return
+   */
   static private int[] sortDescending(int[] list) {
     int largestIndex;
     int temp;
@@ -41,6 +71,13 @@ public class SelectionSort {
     return list;
   }
 
+  /**
+   * 
+   * Finds the array index containing the smallest item.
+   * 
+   * @param list Unsorted array of integers
+   * @return
+   */
   private static int findSmallestIndex(int[] list, int from) {
     int smallestIndex = from;
 
@@ -53,6 +90,13 @@ public class SelectionSort {
     return smallestIndex;
   }
 
+  /**
+   * 
+   * Finds the array index containing the largest item.
+   * 
+   * @param list Unsorted array of integers
+   * @return
+   */
   private static int findLargestIndex(int[] list, int from) {
     int largestIndex = from;
 
