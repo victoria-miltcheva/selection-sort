445e03c9976f1d7c9d1aed658119ca013b5ef97c
Victoria Miltcheva <victoria-m@users.noreply.github.com>
2019-07-14 17:44:43 -0400

    Update SelectionSort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,51 +1,67 @@
 
 public class SelectionSort {
-  
-  int[] sort(int[] list, boolean ascending) {
+
+  protected static int[] sort(int[] list, boolean ascending) {
+    if (list.length == 0) {
+      return new int[0];
+    } else if (list.length == 1) {
+      return list;
+    }
+
     return ascending ? sortAscending(list) : sortDescending(list);
   }
-  
-  private int[] sortAscending(int[] list) {
-    int[] listAscending = new int[list.length];
-    
+
+  static private int[] sortAscending(int[] list) {
+    int smallestIndex;
+    int temp;
+
     for (int i = 0; i < list.length; i++) {
-      listAscending[i] = list[findSmallestIndex(list)];
+      smallestIndex = findSmallestIndex(list, i);
+
+      temp = list[i];
+      list[i] = list[smallestIndex];
+      list[smallestIndex] = temp;
     }
-    
-    return listAscending;
+
+    return list;
   }
-  
-  private int[] sortDescending(int[] list) {
-    int[] listDescending = new int[list.length];
-    
+
+  static private int[] sortDescending(int[] list) {
+    int largestIndex;
+    int temp;
+
     for (int i = 0; i < list.length; i++) {
-      listDescending[i] = list[findLargestIndex(list)];
+      largestIndex = findLargestIndex(list, i);
+
+      temp = list[i];
+      list[i] = list[largestIndex];
+      list[largestIndex] = temp;
     }
-    
-    return listDescending;
+
+    return list;
   }
-  
-  private int findSmallestIndex(int[] list) {
-    int smallestIndex = 0;
-    
-    for (int i = 0; i < list.length; i++) {
+
+  private static int findSmallestIndex(int[] list, int from) {
+    int smallestIndex = from;
+
+    for (int i = from; i < list.length; i++) {
       if (list[smallestIndex] > list[i]) {
         smallestIndex = i;
       }
     }
-    
+
     return smallestIndex;
   }
-  
-  private int findLargestIndex(int[] list) {
-    int largestIndex = 0;
-    
-    for (int i = 0; i < list.length; i++) {
+
+  private static int findLargestIndex(int[] list, int from) {
+    int largestIndex = from;
+
+    for (int i = from; i < list.length; i++) {
       if (list[largestIndex] < list[i]) {
         largestIndex = i;
       }
     }
-    
+
     return largestIndex;
   }
 }
